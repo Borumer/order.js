2b3e7ec2cb439188613f08b6422df795a2dc21ee
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-05-02 21:50:07 -0400

    Capitalized first word in Order
    Created nested function titleCase using the toUpperCase, substring, and toLowerCase methods

--- pizza.js
+++ pizza.js
@@ -1,19 +1,28 @@
 const groupOrder = function(orders) {
+	function titleCase(text) {
+		return text.substring(0,1).toUpperCase() + text.substring(1, text.length).toLowerCase();
+	}
 
 	const pizzaOrder = (topping, crustType, size) => {
-		console.log(`Order: ${size} ${crustType} crust pizza topped with ${topping}`);
+
+		console.log(`Order: ${titleCase(size)} ${crustType.toLowerCase()} crust pizza topped with ${topping}`);
 	};
 
 	const hotDogOrder = (topping) => {
-		console.log(`Order: hot dog with ${topping}`)
+		console.log(`Order: hot dog with ${titleCase(topping)}`)
 	};
 
-	const friesOrder = (quantity, size) {
+	const friesOrder = (quantity, size) => {
 		console.log(`${quantity} ${size} boxes of fries`);
 	}
 	
 	groupOrder.pizzaOrder = pizzaOrder;
 	groupOrder.hotDogOrder = hotDogOrder;
+	groupOrder.friesOrder = friesOrder;
+
+	const addDrink = (name, quantity) => {
+
+	};
 
 	const getSubTotal = (itemCount) => {
 		return 7.5 * itemCount;
@@ -31,14 +40,15 @@ const groupOrder = function(orders) {
 		return total.toFixed(2);
 		
 	};
-
-	console.log(`Total: \$${getTotal(orders.length)}`);
+	if (orders !== null) { // Doesn't log array that has no orders
+		console.log(`\nTotal: \$${getTotal(orders.length)} \n`);
+	}	
 }
 
-groupOrder([]); // Calls empty function for nested function command
+groupOrder(null); // Calls null function for nested function command
 groupOrder(
 	[
-		groupOrder.pizzaOrder("pepperoni", "Thick", "large"),
+		groupOrder.pizzaOrder("pepperoni", "thick", "large"),
 		groupOrder.pizzaOrder("veggies", "deep", "small"),
 		groupOrder.hotDogOrder("mustard"),
 		groupOrder.pizzaOrder("sausage", "Thin", "medium"),
