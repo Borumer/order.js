6b41afc7870ccf9ccad72bd3a2c1310a6efd2e3a
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-16 20:55:33 -0400

    Removed foo.txt and corresponding request

--- docs/main.js
+++
@@ -1,9 +0,0 @@
-var foo = document.getElementById("foo");
-const xhr = new XMLHttpRequest();
-
-xhr.open("GET", "./foo.txt", true);
-
-xhr.onreadystatechange = function() {
-	foo.innerHTML = xhr.responseText;
-};
-xhr.send();
