f90214864fd4c38c6c3b5fd0b61604a316f34ae4
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-23 19:16:00 -0400

    Added quantity and drink input

--- orderIn.js
+++ orderIn.js
@@ -6,11 +6,24 @@ console.log = function(statement) {
 
 function showOrder() {
 	const form = document.querySelector("form");
+	checkNull: try {
+		for (let i = 0; i < Object.keys(form.elements).length; i++) {
+			if(form.elements[i].required && form.elements[i].value === "") {
+				break checkNull;
+			}
+		}
+		groupOrder(
+			[
+				groupOrder[form.orderType.value + "Order"](form.orderTopping.value, form.crustType.value, form.size.value),
+			]
+		);
+	}
+	catch(e) {
 
-	groupOrder(
-		[
-			groupOrder[form.orderType.value + "Order"](form.orderTopping.value, form.crustType.value, form.size.value),
-		]
-	);
+	}
+}
+
+function duplicateForm() {
+	
 }
 
