640e043d3e815df0598caaa123eadde9bac5b59e
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-24 22:05:15 -0400

    Removed showOrder()

--- orderIn.js
+++ orderIn.js
@@ -3,40 +3,16 @@ console.log = function(statement) {
 	outputEl.innerHTML += `${statement} <br>`;
 }
 
-let form = document.querySelector("form");
-const forms = document.querySelectorAll("form");
-
-function showOrder(childEl) {
-	form = childEl.parentNode;
-	checkNull: try {
-		// If the user has not entered a required field don't order		
-		for (let i = 0; i < Object.keys(form.elements).length; i++) {
-			if(form.elements[i].required && form.elements[i].value === "") {
-				break checkNull;
-			}
-		}
-		groupOrder[form.orderType.value + "Order"](
-			{
-				topping: form.orderTopping.value, 
-				crustType: form.crustType.value,
-				size: form.size.value,
-				quantity: form.quantity.value,
-				name: form.drink.value
-			}
-		);
-	}
-	catch(e) {
-
-	}
-}
-
 const firstForm = document.querySelector("form");
-const newForm = firstForm.cloneNode(true);
-const newFields = newForm.childNodes;
+let form = firstForm;
+const forms = document.querySelectorAll("form");
 const afterEl = document.getElementById('writeroot');
 
 let counter = 0;
 function duplicateForm() {
+	const newForm = firstForm.cloneNode(true);
+	const newFields = newForm.childNodes;
+
 	counter++;
 	newForm.id = '';
 	newForm.style.display = 'block';
@@ -49,9 +25,22 @@ function duplicateForm() {
 }
 
 let orderArr = [];
-function orderAll() {
+function orderAll(childEl) {
+	form = childEl.parentNode;
+	checkNull: try {
+		// If the user has not entered a required field don't order		
+		for (let i = 0; i < Object.keys(form.elements).length; i++) {
+			if(form.elements[i].required && form.elements[i].value === "") {
+				break checkNull;
+			}
+		}
+	}
+	catch(e) {
 
+	}	
+	console.log(forms[0].orderType);
 	for (order in forms) {
+		console.log(order.elements);
 		orderArr.push(groupOrder[order.orderType.value + "Order"](
 			{
 				topping: order.topping.value,
@@ -62,5 +51,6 @@ function orderAll() {
 			}
 		));
 	}
+	groupOrder(orderArr);
 }
 
