a05d668fe01ab29182bfdb37257e5c371818ecf5
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-26 20:08:51 -0400

    Divided into separate functions
    Cleared clutter by separating orderAll() into orderFood() and displaySubmitMessage()

--- scripts/orderIn.js
+++ scripts/orderIn.js
@@ -8,29 +8,28 @@ console.log = function(statement) {
 const firstForm = document.querySelector("form");
 const forms = document.querySelectorAll("form"); // Stores nodeList of all the orders the user filled out
 const afterEl = document.getElementById('writeroot'); // Store element that is put after the last order form
-const formsParent = document.getElementById("forms");
+const formsParent = document.getElementById("forms"); // Stores div that contains the form elements and input buttons
 
 // Define changing variables
-let counter = 0;
+let counter = 1;
 
 // Define functions called in input buttons in forms
 function duplicateForm() {
 	const newForm = firstForm.cloneNode(true);
-	const newFields = newForm.childNodes;
-
+	const newFields = newForm.elements;
 	counter++;
 	newForm.id = '';
 	newForm.style.display = 'block';
-	for (let i = 0; i < newFields.length; i++) {
-		const theName = newFields[i].for;
-		if (theName)
-			newFields[i].for = theName + counter;
-	}
+	for (let i = 0; i < forms.length; i++) forms[i].value = "";
 	formsParent.insertBefore(newForm, afterEl);
 }
-function orderAll() {
-	formsParent.innerHTML = "<p style = 'text-align: center; color: black; font-family: `Times New Roman`; font-size: 2em;'> Order submitted. Thank you for ordering! </p>"
-	let orderArr = [];
+function orderFood() {
+	for (let i = 0; i < forms.length; i++) {
+		if (!forms[i]) {
+			forms[i] = "";
+		}
+	}
+
 	checkNull: try {
 		// Loops through all forms
 		for (let i = 0; i < forms.length; i++) {
@@ -48,24 +47,38 @@ function orderAll() {
 				groupOrder[forms[0].orderType.value + "Order"]({
 					topping: forms[0].orderTopping.value,
 					size: forms[0].size.value,
-					crustType: forms[0].crustType.value
+					crustType: forms[0].crustType.value,
+					quantity: forms[0].quantity.value,
+					name: forms[0].drink.value
 				}),
 				groupOrder[forms[1].orderType.value + "Order"]({
 					topping: forms[1].orderTopping.value,
 					size: forms[1].size.value,
-					crustType: forms[1].crustType.value
+					crustType: forms[1].crustType.value,
+					quantity: forms[1].quantity.value,
+					name: forms[1].drink.value
 				}),
 				groupOrder[forms[2].orderType.value + "Order"]({
 					topping: forms[2].orderTopping.value,
 					size: forms[2].size.value,
-					crustType: forms[2].crustType.value
+					crustType: forms[2].crustType.value,
+					quantity: forms[2].quantity.value,
+					name: forms[2].drink.value
 				})
 			]
 		);					
 	}
-	catch(e) {
-
-	}	
+	catch(e) {}	
+}
+function displaySubmitMessage() {
+	formsParent.innerHTML = `<p style = "text-align: center; color: black; font-family: 'Times New Roman'; font-size: 2em;"> Order submitted. Thank you for ordering! </p>`;
+	formsParent.scrollIntoView({ 
+	  behavior: 'smooth' 
+	});	
+}
+function orderAll() {
+	orderFood();
+	displaySubmitMessage();
 }
 function removeOrder(el) {
 	// Call remove method on element to remove
