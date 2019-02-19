a911f7898b72011e06c55a2a4ceb4d37834e4cb9
Varun <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-02-19 12:33:30 -0500

    Put in window.onload method

--- scripts/unaccessibleContent.js
+++ scripts/unaccessibleContent.js
@@ -1,2 +1,3 @@
-document.querySelector(".site-footer").innerHTML += '<span class = "site-footer-disclaimer">Disclaimer: Food not delivered</span>';
-
+window.onload = function() {
+	document.querySelector(".site-footer").innerHTML += '<span class = "site-footer-disclaimer">Disclaimer: Food not delivered</span>';
+}
